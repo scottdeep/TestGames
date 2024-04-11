cdc07aff57c6c3e89ad39e862b7c6d6491e0b540
scottdeep <67732055+scottdeep@users.noreply.github.com>
2024-04-12 01:46:30 +0530

    Create dataspit_transfer.php

---
+++ dataspit_transfer.php
@@ -0,0 +1,8 @@
+<?php
+// Define the URL you want to redirect to
+$redirect_url = "https://magnetcents.com/coupons/database/pushnotifications/data_spit.php";
+
+// Perform the redirect
+header("Location: $redirect_url");
+exit;
+?>
