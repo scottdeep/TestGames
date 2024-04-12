14be33c4e5004e40b3490640f2e521a402b8a15e
scottdeep <67732055+scottdeep@users.noreply.github.com>
2024-04-12 12:44:14 +0530

    Update dataspit_transfer.php

--- dataspit_transfer.php
+++ dataspit_transfer.php
@@ -1,8 +1,14 @@
-<?php
-// Define the URL you want to redirect to
-$redirect_url = "https://magnetcents.com/coupons/database/pushnotifications/data_spit.php";
 
-// Perform the redirect
-header("Location: $redirect_url");
-exit;
+<?php
+// URL of the JSON file
+$json_file_url = 'https://magnetcents.com/coupons/database/pushnotifications/data_spit.php'; // Replace with the actual URL of your JSON file
+
+// Fetch JSON data from the file
+$json_data = file_get_contents($json_file_url);
+
+// Set the appropriate content type for JSON
+header('Content-Type: application/json');
+
+// Output the JSON data in the response
+echo $json_data;
 ?>
